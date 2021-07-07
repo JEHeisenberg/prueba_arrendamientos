db17b628df85dbad19465a26d3b715ed6c6fbd55
JEHeisenberg <stivenbmx201567@gmail.com>
2021-07-07 18:43:55 -0500

    incio de proyecto

---
+++ arrendamientos/src/main/java/com/ceiba/arrendamientos/ArrendamientosApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.arrendamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrendamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrendamientosApplication.class, args);
+	}
+
+}
